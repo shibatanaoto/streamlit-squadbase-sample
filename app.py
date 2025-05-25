18aa782ab67f87c0947644901c0eed8c79965379
shibatanaoto <naoto.shibata510@gmail.com>
2025-05-25 18:08:07 +0900

    pc on2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 
-st.title("Streamlit Squadbase Sample[feature/test] pc1")
+st.title("Streamlit Squadbase Sample[feature/test] pc2")
 
 # File uploader
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
